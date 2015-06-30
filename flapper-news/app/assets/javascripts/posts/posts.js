bdc8a9a36146d5c332c1898637b1c69a3d63cb60
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-06-30 10:50:34 -0430

    Implementación de addComment y upvoteComment

--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -1,13 +1,7 @@
 angular.module('flapperNews')
 .factory('posts', ['$http', function($http){
   var o = {
-    posts: [
-      {title: 'post 1', upvotes: 5, comments: []},
-      {title: 'post 2', upvotes: 2, comments: []},
-      {title: 'post 3', upvotes: 15, comments: []},
-      {title: 'post 4', upvotes: 9, comments: []},
-      {title: 'post 5', upvotes: 4, comments: []},
-    ],
+    posts: [],
   };
 
   o.getAll = function(){
@@ -34,6 +28,19 @@ angular.module('flapperNews')
       return res.data;
     });
   };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments.json', comment);
+  };
+
+  o.upvoteComment = function(post, comment) {
+    return $http.put(
+      '/posts/' + post.id + '/comments/' + comment.id + '/upvote.json')
+      .success(function(data){
+        comment.upvotes += 1;
+      });
+  }
+
   return o;
 }])
 ;
